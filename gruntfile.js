aae59b270e1ef95b3c9cbeeb6eedaf6224416e83
sureshnswamy <sureshnswamy@gmail.com>
2018-07-18 13:55:23 +1200

    first task added to gruntfile

---
+++ gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+
+    concat : {
+      dist : {
+        src: ['components/scripts/*.js'],
+        dest: 'builds/development/js/script.js'
+      }
+    }
+
+
+  }); //initConfig
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+
+  grunt.registerTask('default', ['concat']);
+
+}; //wrapper function
